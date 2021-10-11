d444ee7b3a0d88b97456787de514ca6b4bfeb56c
arrahmanbd <arrahman.bd@outlook.com>
2021-10-11 19:20:08 +0600

    Version :1 (First Project)

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Ask Me Anything'),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                  Colors.blueAccent,
+                  Colors.purple,
+                  Colors.blue
+                ])),
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('img/bg.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: const AskmeMain(),
+        ),
+      ),
+    ),
+  );
+}
+
+class AskmeMain extends StatefulWidget {
+  const AskmeMain({Key? key}) : super(key: key);
+
+  @override
+  _AskmeMainState createState() => _AskmeMainState();
+}
+
+class _AskmeMainState extends State<AskmeMain> {
+  var randAsk = 1;
+  void changeQues() {
+    setState(() {
+      randAsk = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextButton(
+          onPressed: () {
+            changeQues();
+          },
+          child: Image.asset(
+            'img/ball$randAsk.png',
+            height: 300,
+            width: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
